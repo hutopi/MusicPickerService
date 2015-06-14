7b722c38f63c3a57ccb1e96b7684039ae550b489
Hugo Caille <hugo.caille@isen-lille.fr>
2015-06-14 23:13:35 +0200

    Metadata API controllers skeletons

---
+++ MusicPickerService/Controllers/AlbumsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MusicPickerService.Controllers
+{
+    public class AlbumsController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<string> GetByDevice(int device)
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet]
+        public IEnumerable<string> GetByDeviceAndArtist(int device, int artist)
+        {
+            return new string[] { "value3", "value4" };
+        }
+
+        [HttpGet]
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
---
+++ MusicPickerService/Controllers/ArtistsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MusicPickerService.Controllers
+{
+    public class ArtistsController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<string> GetByDevice(int device)
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet]
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
