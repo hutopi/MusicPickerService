7bcf4925b1890b13aa9096c13f347d8c7ef0d8b5
Hugo Caille <hugo.caille@isen-lille.fr>
2015-06-15 01:59:25 +0200

    Albums API listings by device and artist

--- MusicPickerService/Controllers/AlbumsController.cs
+++ MusicPickerService/Controllers/AlbumsController.cs
@@ -4,21 +4,32 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Microsoft.AspNet.Identity.Owin;
+using MusicPickerService.Models;
 
 namespace MusicPickerService.Controllers
 {
     public class AlbumsController : ApiController
     {
-        [HttpGet]
-        public IEnumerable<string> GetByDevice(int device)
+        private ApplicationDbContext db
         {
-            return new string[] { "value1", "value2" };
+            get { return Request.GetOwinContext().Get<ApplicationDbContext>(); }
         }
 
         [HttpGet]
-        public IEnumerable<string> GetByDeviceAndArtist(int device, int artist)
+        public IEnumerable<Album> GetByDevice(int device)
         {
-            return new string[] { "value3", "value4" };
+            return (from d in db.DeviceTracks
+                        where d.DeviceId == device
+                        select d.Track.Album).Distinct().ToList();
+        }
+
+        [HttpGet]
+        public IEnumerable<Album> GetByDeviceAndArtist(int device, int artist)
+        {
+            return (from d in db.DeviceTracks
+                    where d.DeviceId == device && d.Track.Album.ArtistId == artist
+                    select d.Track.Album).Distinct().ToList();
         }
 
         [HttpGet]
--- MusicPickerService/Models/Album.cs
+++ MusicPickerService/Models/Album.cs
@@ -4,21 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
 
 namespace MusicPickerService.Models
 {
+    [DataContract]
     public class Album
     {
         [Key]
+        [DataMember]
         public int Id { get; set; }
 
         [Required]
+        [DataMember]
         public string Name { get; set; }
 
+        [DataMember]
         public uint Year { get; set; }
+        [DataMember]
         public string MbId { get; set; }
 
         [Required]
+        [DataMember]
         public int ArtistId { get; set; }
         public virtual Artist Artist { get; set; }
 
