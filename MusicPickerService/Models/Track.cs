b99f6be82da4d5ec6dcf8a186c0160d805e294dc
Hugo Caille <hugo.caille@isen-lille.fr>
2015-06-15 01:19:29 +0200

    DataContract for Track model

--- MusicPickerService/Models/Track.cs
+++ MusicPickerService/Models/Track.cs
@@ -4,21 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
 
 namespace MusicPickerService.Models
 {
+    [DataContract]
     public class Track
     {
         [Key]
+        [DataMember]
         public int Id { get; set; }
         
         [Required]
+        [DataMember]
         public string Name { get; set; }
 
+        [DataMember]
         public int Number { get; set; }
+        [DataMember]
         public string MbId { get; set; }
 
         [Required]
+        [DataMember]
         public int AlbumId { get; set; }
         public virtual Album Album { get; set; }
 
