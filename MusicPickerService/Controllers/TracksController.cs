6e7273411d665fde39c08732e766dd521a41cdf0
Hugo Caille <hugo.caille@isen-lille.fr>
2015-06-15 01:43:36 +0200

    Tracks API listings by device and album

--- MusicPickerService/Controllers/TracksController.cs
+++ MusicPickerService/Controllers/TracksController.cs
@@ -4,21 +4,32 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Microsoft.AspNet.Identity.Owin;
+using MusicPickerService.Models;
 
 namespace MusicPickerService.Controllers
 {
     public class TracksController : ApiController
     {
-        [HttpGet]
-        public IEnumerable<string> GetByDevice(int device)
+        private ApplicationDbContext db
         {
-            return new string[] { "value1", "value2" };
+            get { return Request.GetOwinContext().Get<ApplicationDbContext>(); }
         }
 
         [HttpGet]
-        public IEnumerable<string> GetByDeviceAndAlbum(int device, int album)
+        public IEnumerable<Track> GetByDevice(int device)
         {
-            return new string[] { "value3", "value4" };
+            return (from d in db.DeviceTracks
+                    where d.DeviceId == device
+                    select d.Track).ToList();
+        }
+
+        [HttpGet]
+        public IEnumerable<Track> GetByDeviceAndAlbum(int device, int album)
+        {
+            return (from d in db.DeviceTracks
+                where d.DeviceId == device && d.Track.AlbumId == album
+                select d.Track).ToList();
         }
 
         [HttpGet]
